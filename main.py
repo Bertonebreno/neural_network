fix(backprop): Backpropagation now works

main.py
@@ -1,26 +1,41 @@
-from methods.feedforward import check_test_problem, generate_test_problem
+from methods.feedforward import check_test_problem, feedforward, generate_test_problem
 from methods.gradient_descendent import gradient_descendent
-from utils import generate_theta
+from utils import generate_theta, read_MNIST_data, plot_theta
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 num_of_neurons = [2, 1]
 num_of_layers = len(num_of_neurons)
 
+(
+    training_images,
+    training_labels,
+    validation_images,
+    validation_labels,
+    test_images,
+    test_labels,
+) = read_MNIST_data()
+
 initial_theta, initial_theta0 = generate_theta(num_of_neurons)
-input_data, expected_output = generate_test_problem(10)
+input_data, expected_output = generate_test_problem(100)
 theta, theta0, numeric_theta, numeric_theta0 = gradient_descendent(
     initial_theta,
     initial_theta0,
     input_data,
     expected_output,
-    learning_rate=0.1,
+    learning_rate=0.01,
     num_of_iterations=10000,
-    calc_numeric=True,
+    calc_numeric=False,
 )
 
-handpicked_theta = [np.array([]), np.array([[10, -10]])] 
+handpicked_theta = [np.array([]), np.array([[10, -10]])]
 handpicked_theta0 = [np.array([]), np.array([[0]])]
-print("Backprop test: ", check_test_problem(handpicked_theta, handpicked_theta0, number_of_tries=1000))
+
+print(
+    "Handpick test: ",
+    check_test_problem(handpicked_theta, handpicked_theta0, number_of_tries=1000),
+)
 print("Backprop test: ", check_test_problem(theta, theta0, number_of_tries=1000))
 print(
     "Numeric test: ",

methods/backpropagation.py
@@ -26,6 +26,6 @@ def backpropagation(
         )
 
     for layer in range(1, num_of_layers):
-        gradient[layer] += np.dot(activated_neurons[layer], error[layer])
+        gradient[layer] += np.dot(error[layer], activated_neurons[layer - 1].T)
 
     return gradient, error

methods/feedforward.py
@@ -10,8 +10,8 @@ def feedforward(
 ) -> Tuple[List[np.ndarray], List[np.ndarray]]:
     num_of_layers = len(theta)
 
-    activated_neurons = [np.array([])] * num_of_layers
-    neurons = [np.array([])] * num_of_layers
+    activated_neurons: List[np.ndarray] = [np.array([])] * num_of_layers
+    neurons: List[np.ndarray] = [np.array([])] * num_of_layers
 
     activated_neurons[0] = input
     for layer in range(1, num_of_layers):
@@ -23,7 +23,7 @@ def feedforward(
     return activated_neurons, neurons
 
 
-def generate_test_problem(num_of_examples: int) -> Tuple[np.ndarray, np.ndarray]:
+def generate_test_problem(num_of_examples: int) -> Tuple[List, List]:
     input = np.array([np.random.random((2, 1)) for i in range(num_of_examples)])
     expected_output = np.array([1 if row[0] > row[1] else 0 for row in input])
 
@@ -38,10 +38,10 @@ def check_test_problem(
     for i in range(number_of_tries):
         input, expected_output = generate_test_problem(1)
 
-        activated_neurons, _ = feedforward(theta, theta0, input)
+        activated_neurons, _ = feedforward(theta, theta0, input[0])
 
-        answer = True if activated_neurons[-1] > 0.5 else False
-        if answer == expected_output:
+        answer = 1 if activated_neurons[-1] > 0.5 else 0
+        if answer == expected_output[0]:
             trues += 1
         else:
             falses += 1

methods/gradient_descendent.py
@@ -6,6 +6,7 @@ import numpy as np
 from methods.backpropagation import backpropagation
 from methods.feedforward import feedforward
 from methods.numerical_gradient import numerical_gradient
+from utils import cost
 
 
 def gradient_descendent(
@@ -16,7 +17,7 @@ def gradient_descendent(
     learning_rate: float,
     num_of_iterations: int,
     calc_numeric: bool = False,
-) -> Tuple[List[np.ndarray], List[np.ndarray], List[np.ndarray], List[np.ndarray]]:
+) -> Tuple[List[np.ndarray], List[np.ndarray], List[np.ndarray], List[np.ndarray], List[float]]:
 
     theta: List[np.ndarray] = deepcopy(initial_theta)
     theta0: List[np.ndarray] = deepcopy(initial_theta0)
@@ -27,13 +28,14 @@ def gradient_descendent(
     num_of_examples = len(input_data)
 
     for i in range(num_of_iterations):
-        if i % int(num_of_iterations/10) == 0:
-            print(100*"\n")
+        if i % int(num_of_iterations / 10) == 0:
+            print(100 * "\n")
             print(f"Progress: {i}/{num_of_iterations}")
 
         gradient, error = get_gradient_shaped_array(theta, theta0)
         numeric_gradient, numeric_error = get_gradient_shaped_array(theta, theta0)
 
+        total_cost = 0
         for input_row, expected_output_row in zip(input_data, expected_output):
             activated_neurons, neurons = feedforward(theta, theta0, input_row)
 
@@ -42,26 +44,23 @@ def gradient_descendent(
             )
             if calc_numeric:
                 numeric_gradient_row, numeric_error_row = numerical_gradient(
-                    theta, theta0, input_row, expected_output_row
+                    numeric_theta, numeric_theta0, input_row, expected_output_row
                 )
 
             for layer in range(1, num_of_layers):
-                gradient[layer] += gradient_row[layer] / num_of_examples
-                error[layer] += error_row[layer] / num_of_examples
+                gradient[layer] -= gradient_row[layer] / num_of_examples
+                error[layer] -= error_row[layer] / num_of_examples
 
                 if calc_numeric:
                     numeric_gradient[layer] -= (
                         numeric_gradient_row[layer] / num_of_examples
                     )
                     error[layer] -= numeric_error_row[layer] / num_of_examples
+            
+            iteration_cost = 0
+
 
         for layer in range(1, num_of_layers):
-            if i % int(num_of_iterations/10) == 0:
-                print(f"gradient[{layer}]: {gradient[layer]}")
-                print(f"error[{layer}]: {error[layer]}")
-                print(f"numeric_gradient[{layer}]: {numeric_gradient[layer]}")
-                print(f"numeric_error[{layer}]: {numeric_error[layer]}")
-
             theta[layer] += learning_rate * gradient[layer]
             theta0[layer] += learning_rate * error[layer]
             numeric_theta[layer] += learning_rate * numeric_gradient[layer]

utils.py
@@ -1,6 +1,8 @@
 from typing import List, Tuple
 
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def generate_theta(
@@ -12,8 +14,10 @@ def generate_theta(
     theta0: List[np.ndarray] = [np.array([])] * num_of_layers  # Biases
 
     for layer in range(1, num_of_layers):
-        theta[layer] = np.random.rand(num_of_neurons[layer], num_of_neurons[layer - 1])
-        theta0[layer] = np.random.rand(num_of_neurons[layer], 1)
+        epi = (6**(1/2))/(num_of_neurons[layer] + num_of_neurons[layer - 1])**(1/2)
+
+        theta[layer] = np.random.rand(num_of_neurons[layer], num_of_neurons[layer - 1]) * epi
+        theta0[layer] = np.random.rand(num_of_neurons[layer], 1) * epi
 
     return theta, theta0
 
@@ -28,3 +32,54 @@ def d_sigmoid(z: np.ndarray) -> np.ndarray:
 
 def cost(output: np.ndarray, expected_output: np.ndarray) -> float:
     return np.sum((output - expected_output) ** 2 / 2)
+
+
+def plot_theta(theta: List[np.ndarray]) -> None:
+    fig, axis = plt.subplots(4, 8, figsize=(8, 8))
+    image_number = 0
+    for i in range(4):
+        for j in range(8):
+            theta_Li = theta[1][image_number]
+            axis[i, j].imshow(
+                (theta_Li - np.min(theta_Li) / np.ptp(theta_Li)).reshape(
+                    20, 20, order="F"
+                ),
+                cmap="hot",
+            )
+            axis[i, j].axis("off")
+            image_number += 1
+    plt.show()
+
+
+def read_MNIST_data() -> Tuple[
+    List[np.ndarray],
+    List[np.ndarray],
+    List[np.ndarray],
+    List[np.ndarray],
+    List[np.ndarray],
+    List[np.ndarray],
+]:
+    images = pd.read_csv(
+        "data/imageMNIST.csv", delimiter="|", quotechar='"', header=None
+    ).values.tolist()
+    labels = pd.read_csv(
+        "data/labelMNIST.csv", delimiter=",", quotechar='"', header=None
+    ).values.tolist()
+
+    training_images = images[:3000]
+    training_labels = labels[:3000]
+
+    validation_images = images[3000:4001]
+    validation_labels = labels[3000:4001]
+
+    test_images = images[4000 : 5000 - 1]
+    test_labels = labels[4000 : 5000 - 1]
+
+    return (
+        training_images,
+        training_labels,
+        validation_images,
+        validation_labels,
+        test_images,
+        test_labels,
+    )